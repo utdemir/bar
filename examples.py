661829e67fe95ca1d56fc769b4d08417857b2fc6
Utku Demir <utdemir@gmail.com>
2014-06-18 17:13:06 +0300

    Add cancel mathod

--- examples.py
+++ examples.py
@@ -2,7 +2,12 @@ import time
 
 from bar import Bar
 
-with Bar(subject="Reticulating splines... ", bar_max=4524, end=True) as bar:
-    for i in range(4524):
-        time.sleep(0.0005)
+for i in ("block", "shades", "ascii"):
+    with Bar(subject="Reticulating splines... ", bar_max=3000, end=True, chars=i) as bar:
+        for i in range(3000):
+            time.sleep(0.001)
+            bar.step()
+
+with Bar(subject="Reticulating splines... ", bar_max=1000000, end=True) as bar:
+    for i in range(1000000):
         bar.step()
