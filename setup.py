65dd7f56878560bea232a648951662566a4e5e11
Utku Demir <utdemir@gmail.com>
2014-07-05 22:28:49 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='bar',
-      version='0.1.0',
+      version='0.2.0',
       description='Configurable progress bars/status monitors for Python console applications.',
       url='https://github.com/utdemir/bar',
       author='Utku Demir',
